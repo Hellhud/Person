880b17951e0f052b4e81d823695298bc51123ecf
Hellhound <hardoneloves@gmail.com>
2022-04-04 23:48:39 +0300

    Добавьте файлы проекта.

---
+++ Person/Person.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+class Man {
+public:
+	char name[50];
+	int age;
+	int pol;
+	int weight;
+	void changeName();
+	void changeWeight(int manWeight);
+};
+
+class Student : public Man
+{
+	int year;
+public:
+	Student(char* a, int Age, int Pol, int Weigh, int Year);
+	void incYear();
+	void changeYear();
+	void show();
+};
+
+void Man::changeName()
+{
+	char a[50];
+	cout << "Input name: \n";
+	gets(a);
+	memset(name, ' ', strlen(name) + 1);
+	strcpy(name, a);
+}
+
+void Man::changeWeight(int manWeight)
+{
+	cout << "Input weight: \n";
+	cin >> weight;
+}
+
+Student::Student(char* a, int Age, int Pol, int Weigh, int Year)
+{
+	strcpy(name, a);
+	age = Age;
+	pol = Pol;
+	weight = Weigh;
+	year = Year;
+}
+
+void Student::incYear()
+{
+	int Inc;
+	cout << "Add year :\n";
+	cin >> Inc;
+	year += Inc;
+}
+
+void Student::changeYear()
+{
+	cout << "Input year: \n";
+	cin >> year;
+}
